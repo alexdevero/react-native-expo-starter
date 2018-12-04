247d3eabb06450aca1fb1961be4db80b10da76e9
Alex Devero <deveroalex@gmail.com>
2018-12-04 05:55:24 +0100

    Add navigation component

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Platform } from 'react-native'
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+
+import TabBarIcon from '../components/TabBarIcon'
+import HomeScreen from '../screens/HomeScreen'
+import AboutScreen from '../screens/AboutScreen'
+import SettingsScreen from '../screens/SettingsScreen'
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen
+})
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-home` : 'md-home'}
+    />
+  )
+}
+
+const AboutStack = createStackNavigator({
+  About: AboutScreen
+})
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen
+})
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  )
+}
+
+AboutStack.navigationOptions = {
+  tabBarLabel: 'About',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-information-circle' : 'md-information-circle'}
+    />
+  )
+}
+
+export default createBottomTabNavigator({
+  HomeStack,
+  AboutStack,
+  SettingsStack
+})
